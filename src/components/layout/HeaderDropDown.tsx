f45d511f27623e019565dea9984f7e2beadd0f3d
Chuckie <larssalling98@gmail.com>
2020-02-06 20:25:26 +0100

    Add micro interaction on hover

--- src/components/layout/HeaderDropDown.tsx
+++ src/components/layout/HeaderDropDown.tsx
@@ -8,9 +8,14 @@ const HeaderDropDown = () => {
 
   const user = useUserState();
 
+  const habboFigure = {
+    backgroundImage: `url("https://www.habbo.nl/habbo-imaging/avatarimage?figure=${process.env.REACT_APP_HABBO_FIGURE}&size=s")`,
+    backgroundPositionY: "-4px",
+    backgroundPositionX: "1px"
+  };
   return (
     <div
-      className="bg-fadedwhite-100 rounded shadow relative  inline-block w-40 "
+      className="hover:bg-fadedwhite-100 cursor-pointer rounded relative  inline-block w-40 "
       onMouseEnter={() => setVisible(!visible)}
       onMouseLeave={() => setVisible(!visible)}
     >
@@ -19,11 +24,21 @@ const HeaderDropDown = () => {
           visible ? " rounded-t" : " rounded"
         } border-gray-400   z-50`}
       >
-        <button className="flex p-1">
-          <ProfilePicture figure={process.env.REACT_APP_HABBO_FIGURE} />
-          <span className="text-white self-center mx-1 font-semibold text-sm">
-            {user.username && user.username}
-          </span>
+        <button className="flex p-1 w-full justify-between">
+          <div
+            className="h-8 w-8 bg-center flex-shrink-0 bg-no-repeat rounded bg-gray-200"
+            style={habboFigure}
+          ></div>
+          <div className="flex flex-col w-full self-center">
+            <span className="text-white text-center flex-grow self-center mx-1 font-semibold text-sm">
+              {user.username && user.username}
+            </span>
+            <div
+              className={`${visible ? "h-2" : "h-0"} overflow-hidden flex justify-center bg-fade`}
+            >
+              <i className={`fas fa-caret-down self-center text-xs text-fadedwhite-500 mr-2`}></i>
+            </div>
+          </div>
         </button>
       </div>
       {visible && (
